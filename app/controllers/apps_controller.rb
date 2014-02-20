5520b9061e39d1403a5df52d2f0350ce1ebf798e
Timothy Mecklem <timothy@mecklem.com>
2014-02-19 23:13:25 -0500

    Parsing all the files all the time. Performance can come later in the form of indexing

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -2,7 +2,9 @@ class AppsController < ApplicationController
   def list
     dir = Rails.root.join('public','apps')
     FileUtils.mkdir_p(dir)
-    @list = Dir.entries(dir).reject { |entry| entry.starts_with?('.') }
+    #@list = Dir.entries(dir).reject { |entry| entry.starts_with?('.') }
+    root_dir = Rails.root.join('public','apps')
+    @list = app_names(root_dir)
   end
 
   def show_versions
@@ -22,10 +24,23 @@ class AppsController < ApplicationController
     Dir.glob("#{dir}/**/*").reject { |entry| !entry.upcase.end_with?('IPA') }
   end
 
+  def app_names(root_dir)
+    everything = all_plists(root_dir).collect do |plist|
+      plist["CFBundleDisplayName"]
+    end
+    everything.uniq
+  end
+
   def app_plists(app_name)
-    dir = Rails.root.join('public','apps',app_name)
-    plists = file_list(dir).collect do |ipa_file|
-      plist_info(dir.join(ipa_file))
+    root_dir = Rails.root.join('public','apps')
+    all_plists(root_dir).select do |plist|
+      app_name.eql?(plist["CFBundleDisplayName"])
+    end
+  end
+
+  def all_plists(root_dir)
+    plists = file_list(root_dir).collect do |ipa_file|
+      plist_info(root_dir.join(ipa_file))
     end
     plists.compact
   end
