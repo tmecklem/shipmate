05f66160f101ac5c3100712e3e9c87910e3c8e75
Timothy Mecklem <timothy@mecklem.com>
2014-03-17 19:41:47 -0400

    Removing duplication

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -8,30 +8,24 @@ class AppsController < ApplicationController
   attr_accessor :android_dir
 
   def initialize
-    @ios_dir = Shipmate::Application.config.ios_dir
-    FileUtils.mkdir_p(@ios_dir)
-    @android_dir = Shipmate::Application.config.android_dir
-    FileUtils.mkdir_p(@android_dir)
+    @ios_dir,@android_dir = app_dirs = [Shipmate::Application.config.ios_dir,Shipmate::Application.config.android_dir]
+    app_dirs.each do |app_dir|
+      FileUtils.mkdir_p(app_dir)
+    end
     super
   end
 
   def index
-    @ios_app_names = ios_app_names
-    @android_app_names = android_app_names
+    @ios_app_names = app_names(@ios_dir)
+    @android_app_names = app_names(@android_dir)
   end
 
-  def ios_app_names 
+  def app_names(app_dir)
+    app_type = app_dir == @ios_dir ? IOS_APP_TYPE : ANDROID_APP_TYPE
     subdirectories(@ios_dir).sort.select do |app_name|
       app_builds = self.app_builds(app_name,@ios_dir,IOS_APP_TYPE)
       (app_builds.first && app_builds.first.supports_device?(@device_type)) || @device_type == :desktop
     end
   end
-
-  def android_app_names
-    subdirectories(@android_dir).sort.select do |app_name|
-      app_builds = self.app_builds(app_name,@android_dir,ANDROID_APP_TYPE)
-      (app_builds.first && app_builds.first.supports_device?(@device_type)) || @device_type == :desktop
-    end
-  end 
 
 end
