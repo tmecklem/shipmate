669e180e2b2fd7e6fcc36f650d311869379c962d
Brandon Brock <bbrock@medplus.com>
2014-03-19 13:15:34 -0400

    Adding an app builds controller for android.

--- app/controllers/android_apps_controller.rb
+++ app/controllers/android_apps_controller.rb
@@ -3,6 +3,12 @@ require 'shipmate/apk_parser'
 class AndroidAppsController < AppsController
 
   def list_app_builds
+    @app_name = params[:app_name]
+    @app_release = params[:app_release]
+
+    @app_builds = self.app_builds(@app_name, @android_dir, ANDROID_APP_TYPE).select do |app_build|
+      app_build.release.eql?(@app_release)
+    end
 
   end
 
--- spec/controllers/android_apps_controller_spec.rb
+++ spec/controllers/android_apps_controller_spec.rb
@@ -39,7 +39,46 @@ describe AndroidAppsController do
       get :list_app_releases, :app_name => app_name
       expect(assigns[:most_recent_build_hash]).to eq({'1.2.6'=>AppBuild.new(android_dir,app_name,'1.2.6.0'), '1.2.4'=>AppBuild.new(android_dir,app_name,'1.2.4.10'), '1.2.2'=>AppBuild.new(android_dir,app_name,'1.2.2.0')})
     end
+  end
 
+  describe 'GET #list_app_builds' do
+    before (:each) do
+      FileUtils.mkdir_p(android_dir.join('Vanilla','1.2.0.14'))
+      FileUtils.mkdir_p(android_dir.join('Vanilla','1.2.0.12'))
+      FileUtils.mkdir_p(android_dir.join('Vanilla','1.2.0.1'))
+      FileUtils.mkdir_p(android_dir.join('Vanilla','1.2.3.goofy'))
+      FileUtils.mkdir_p(android_dir.join('Vanilla','1.2.0.2'))
+    end
+
+    after(:each) do
+      FileUtils.rm_r(android_dir.join('Vanilla'))
+    end
+
+    it 'returns a 200' do
+      get :list_app_builds, :app_name => 'Vanilla', :app_release => '1.2.0'
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it 'assembles a reverse version sorted list of builds within a release' do
+      app_name = 'Vanilla'
+      get :list_app_builds, :app_name => app_name, :app_release => '1.2.0'
+      expect(assigns[:app_builds]).to eq [AppBuild.new(android_dir,app_name,'1.2.0.14'),AppBuild.new(android_dir,app_name,'1.2.0.12'),AppBuild.new(android_dir,app_name,'1.2.0.2'),AppBuild.new(android_dir,app_name,'1.2.0.1')]
+    end
+
+    it 'assigns the app name' do
+      get :list_app_builds, :app_name => 'Vanilla', :app_release => '1.2.0'
+      expect(assigns[:app_name]).to eq 'Vanilla'
+    end
+
+    #before(:each) do
+    #  FileUtils.mkdir_p(android_dir.join('Christmas Conspiracy','4.2.55'))
+    #  FileUtils.cp(Rails.root.join('spec','fixtures','ChristmasConspiracy.apk'), ios_dir.join('Christmas Conspiracy','4.2.55','Christmas Conspiracy-4.2.55.apk'))
+    #end
+    #
+    #after(:each) do
+    #  FileUtils.rm_rf(android_dir.join('Christmas Conspiracy'))
+    #end
   end
 
 end
